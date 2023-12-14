f5edc19696792cf8cf80a9ded17e66918c4f3aaa
Abdelhadi Chougar <chougar.abdelhadi@gmail.com>
2023-12-14 11:39:11 +0100

    jwt token generate

--- src/main/java/fr/vde/springbootsecurityjwtauth/DTO/request/LoginRequest.java
+++ src/main/java/fr/vde/springbootsecurityjwtauth/DTO/request/LoginRequest.java
@@ -1,4 +1,18 @@
 package fr.vde.springbootsecurityjwtauth.DTO.request;
 
 public class LoginRequest {
+
+  private Object message = "connexion reussi";
+
+  public LoginRequest(Object message) {
+    this.message = message;
+  }
+
+  public Object getMessage() {
+    return message;
+  }
+
+  public void setMessage(Object message) {
+    this.message = message;
+  }
 }
